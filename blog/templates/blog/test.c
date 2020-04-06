20782f1620fc0f166ca36b057921ccd1d593bc6d
Rizka Shinta W <rizkashintaw@gmail.com>
2020-04-06 13:15:42 +0700

    Added view and template for detailed blog post as well as CSS for the site.

---
+++ blog/templates/blog/test.c
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int a, b, c;
+  cout<<"Masukan nilai A: "; cin>>a;
+  cout<<"Masukan nilai B: "; cin>>b;
+
+  c = a+b;
+
+  cout<<"Nilai A+B= "<<c;
+
+}
